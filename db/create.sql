78a384195fdab9f166c09dbebe11ec6a9cf9b352
Paul B. Krause <paul.b.krause@gmail.com>
2026-01-09 15:57:16 -0500

    Starting to put the project together

---
+++ db/create.sql
@@ -0,0 +1,53 @@
+-- =====================================================================================================================
+-- Create a simple schema for playing around with databases
+-- =====================================================================================================================
+
+CREATE TABLE account
+( pkey INTEGER PRIMARY KEY
+, acct_id VARCHAR (64) UNIQUE NOT NULL
+, acct_name VARCHAR (256) UNIQUE NOT NULL
+);
+
+CREATE TABLE product
+( pkey INTEGER PRIMARY KEY
+, prod_id VARCHAR(32) UNIQUE NOT NULL
+, prod_name VARCHAR(256) UNIQUE NOT NULL
+);
+
+CREATE TABLE product_ver
+( pkey INTEGER PRIMARY KEY
+, fkey_prod INTEGER
+, maj_ver INTEGER NOT NULL
+, min_ver INTEGER NOT NULL
+, rel_ver INTEGER
+, bld_ver INTEGER
+, bld_tag VARCHAR(128)
+);
+
+CREATE TABLE service
+( pkey INTEGER PRIMARY KEY
+, svc_id VARCHAR (32) UNIQUE NOT NULL
+, svc_name VARCHAR (128) UNIQUE NOT NULL
+);
+
+CREATE TABLE service_ver
+( pkey INTEGER PRIMARY KEY
+, fkey_svc INTEGER NOT NULL
+, maj_ver INTEGER NOT NULL
+, min_ver INTEGER NOT NULL
+, rel_ver INTEGER
+, bld_ver INTEGER
+, bld_tag VARCHAR(128)
+);
+
+CREATE TABLE request
+( pkey INTEGER PRIMARY KEY
+, req_type VARCHAR(64)
+, req_start VARCHAR(64)
+, req_status VARCHAR(64)
+);
+
+CREATE TABLE task
+( pkey INTEGER PRIMARY KEY
+, fkey_req INTEGER NOT NULL
+);
---
+++ db/insert.sql
@@ -0,0 +1,114 @@
+-- =====================================================================================================================
+-- Load Basic SQL Data for testing and dev
+-- =====================================================================================================================
+
+-- A Couple of accounts
+
+INSERT INTO account (pkey, acct_id, acct_name)
+VALUES (0, 'ACT1', 'First Account');
+
+INSERT INTO account (pkey, acct_id, acct_name)
+VALUES (1, 'ACT2', 'Second Account');
+
+-- Some Products
+
+INSERT INTO product (pkey, prod_id, prod_name)
+VALUES (0, 'PIXTORE', 'Photo Storage');
+
+INSERT INTO product (pkey, prod_id, prod_name)
+VALUES (1, 'UTUNE', 'Music Library and Storage');
+
+INSERT INTO product (pkey, prod_id, prod_name)
+VALUES (2, 'WEBED', 'Web Editor');
+
+-- Some Product Versions
+
+-- PIXTORE
+
+INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
+VALUES (0, (SELECT pkey FROM product WHERE prod_id = 'PIXTORE'), 0, 1);
+
+INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
+VALUES (1, (SELECT pkey FROM product WHERE prod_id = 'PIXTORE'), 0, 2);
+
+INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver, bld_tag)
+VALUES (2, (SELECT pkey FROM product WHERE prod_id = 'PIXTORE'), 1, 0, 'beta');
+
+INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
+VALUES (3, (SELECT pkey FROM product WHERE prod_id = 'PIXTORE'), 1, 0);
+
+-- UTUNE
+
+INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
+VALUES (4, (SELECT pkey FROM product WHERE prod_id = 'UTUNE'), 0, 1);
+
+INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver, bld_tag)
+VALUES (5, (SELECT pkey FROM product WHERE prod_id = 'UTUNE'), 1, 0, 'beta');
+
+INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
+VALUES (6, (SELECT pkey FROM product WHERE prod_id = 'UTUNE'), 1, 0);
+
+INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
+VALUES (7, (SELECT pkey FROM product WHERE prod_id = 'UTUNE'), 1, 1);
+
+INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
+VALUES (8, (SELECT pkey FROM product WHERE prod_id = 'UTUNE'), 1, 2);
+
+-- WEBED
+
+INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
+VALUES (9, (SELECT pkey FROM product WHERE prod_id = 'WEBED'), 0, 1);
+
+INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver, bld_tag)
+VALUES (10, (SELECT pkey FROM product WHERE prod_id = 'WEBED'), 1, 0, 'beta');
+
+INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
+VALUES (11, (SELECT pkey FROM product WHERE prod_id = 'WEBED'), 1, 0);
+
+INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
+VALUES (12, (SELECT pkey FROM product WHERE prod_id = 'WEBED'), 1, 1);
+
+INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver, bld_tag)
+VALUES (13, (SELECT pkey FROM product WHERE prod_id = 'WEBED'), 2, 0, 'beta');
+
+INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
+VALUES (14, (SELECT pkey FROM product WHERE prod_id = 'WEBED'), 2, 0);
+
+-- Some Services
+
+INSERT INTO service (pkey, svc_id, svc_name)
+VALUES (0, 'AUTH', 'Authorization Service');
+
+INSERT INTO service (pkey, svc_id, svc_name)
+VALUES (1, 'OSTORE', 'Data/Object Storage');
+
+INSERT INTO service (pkey, svc_id, svc_name)
+VALUES (2, 'REPT', 'Reporting Service');
+
+INSERT INTO service (pkey, svc_id, svc_name)
+VALUES (3, 'WEBUI', 'Web UI Service');
+
+INSERT INTO service (pkey, svc_id, svc_name)
+VALUES (4, 'SCHED', 'CPM Scheduling Service');
+
+-- Some Services Version
+
+INSERT INTO service_ver (pkey, fkey_svc, maj_ver, min_ver)
+VALUES (0, (SELECT pkey FROM service WHERE svc_id = 'AUTH'), 1, 0);
+
+INSERT INTO service_ver (pkey, fkey_svc, maj_ver, min_ver)
+VALUES (1, (SELECT pkey FROM service WHERE svc_id = 'OSTORE'), 1, 0);
+
+INSERT INTO service_ver (pkey, fkey_svc, maj_ver, min_ver)
+VALUES (2, (SELECT pkey FROM service WHERE svc_id = 'REPT'), 1, 0);
+
+INSERT INTO service_ver (pkey, fkey_svc, maj_ver, min_ver)
+VALUES (3, (SELECT pkey FROM service WHERE svc_id = 'WEBUI'), 1, 0);
+
+INSERT INTO service_ver (pkey, fkey_svc, maj_ver, min_ver)
+VALUES (4, (SELECT pkey FROM service WHERE svc_id = 'SCHED'), 1, 0);
+
+
+
+
+
