0fbada0db8c7564f3e1c4f02dea3320e8afe9062
Paul B. Krause <paul.b.krause@gmail.com>
2026-01-31 11:33:08 -0500

    Starting to simplify sample data

--- db/cplane.sample.insert.sql
+++ db/cplane.sample.insert.sql
@@ -22,63 +22,19 @@ INSERT INTO product (pkey, prod_id, prod_name)
 VALUES (0, 'CPLANE', 'Control Plane');
 
 INSERT INTO product (pkey, prod_id, prod_name)
-VALUES (1, 'UTUNE', 'Music Library and Storage');
-
-INSERT INTO product (pkey, prod_id, prod_name)
-VALUES (2, 'WEBED', 'Web Editor');
+VALUES (1, 'AION', 'Schedule Management Application');
 
 -- Some Product Versions
 
--- PIXTORE
+-- CPLANE
 
 INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
 VALUES (0, (SELECT pkey FROM product WHERE prod_id = 'CPLANE'), 0, 0);
 
--- INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
--- VALUES (1, (SELECT pkey FROM product WHERE prod_id = 'PIXTORE'), 0, 2);
-
--- INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver, bld_tag)
--- VALUES (2, (SELECT pkey FROM product WHERE prod_id = 'PIXTORE'), 1, 0, 'beta');
-
--- INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
--- VALUES (3, (SELECT pkey FROM product WHERE prod_id = 'PIXTORE'), 1, 0);
-
--- UTUNE
+-- AION
 
 INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
-VALUES (4, (SELECT pkey FROM product WHERE prod_id = 'UTUNE'), 0, 1);
-
-INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver, bld_tag)
-VALUES (5, (SELECT pkey FROM product WHERE prod_id = 'UTUNE'), 1, 0, 'beta');
-
-INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
-VALUES (6, (SELECT pkey FROM product WHERE prod_id = 'UTUNE'), 1, 0);
-
-INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
-VALUES (7, (SELECT pkey FROM product WHERE prod_id = 'UTUNE'), 1, 1);
-
-INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
-VALUES (8, (SELECT pkey FROM product WHERE prod_id = 'UTUNE'), 1, 2);
-
--- WEBED
-
-INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
-VALUES (9, (SELECT pkey FROM product WHERE prod_id = 'WEBED'), 0, 1);
-
-INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver, bld_tag)
-VALUES (10, (SELECT pkey FROM product WHERE prod_id = 'WEBED'), 1, 0, 'beta');
-
-INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
-VALUES (11, (SELECT pkey FROM product WHERE prod_id = 'WEBED'), 1, 0);
-
-INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
-VALUES (12, (SELECT pkey FROM product WHERE prod_id = 'WEBED'), 1, 1);
-
-INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver, bld_tag)
-VALUES (13, (SELECT pkey FROM product WHERE prod_id = 'WEBED'), 2, 0, 'beta');
-
-INSERT INTO product_ver (pkey, fkey_prod, maj_ver, min_ver)
-VALUES (14, (SELECT pkey FROM product WHERE prod_id = 'WEBED'), 2, 0);
+VALUES (4, (SELECT pkey FROM product WHERE prod_id = 'AION'), 0, 1);
 
 -- Some Services
 
@@ -86,16 +42,10 @@ INSERT INTO service (pkey, svc_id, svc_name)
 VALUES (0, 'AUTH', 'Authorization Service');
 
 INSERT INTO service (pkey, svc_id, svc_name)
-VALUES (1, 'OSTORE', 'Data/Object Storage');
+VALUES (1, 'SHIFTS', 'Shift Management Service (the main AION service)');
 
 INSERT INTO service (pkey, svc_id, svc_name)
-VALUES (2, 'REPT', 'Reporting Service');
-
-INSERT INTO service (pkey, svc_id, svc_name)
-VALUES (3, 'WEBUI', 'Web UI Service');
-
-INSERT INTO service (pkey, svc_id, svc_name)
-VALUES (4, 'SCHED', 'CPM Scheduling Service');
+VALUES (2, 'CPUI', 'Control Plane UI');
 
 -- Some Services Version
 
@@ -103,32 +53,26 @@ INSERT INTO service_ver (pkey, fkey_svc, maj_ver, min_ver)
 VALUES (0, (SELECT pkey FROM service WHERE svc_id = 'AUTH'), 1, 0);
 
 INSERT INTO service_ver (pkey, fkey_svc, maj_ver, min_ver)
-VALUES (1, (SELECT pkey FROM service WHERE svc_id = 'OSTORE'), 1, 0);
+VALUES (1, (SELECT pkey FROM service WHERE svc_id = 'SHIFTS'), 0, 0);
 
 INSERT INTO service_ver (pkey, fkey_svc, maj_ver, min_ver)
-VALUES (2, (SELECT pkey FROM service WHERE svc_id = 'REPT'), 1, 0);
+VALUES (2, (SELECT pkey FROM service WHERE svc_id = 'CPUI'), 0, 0);
 
-INSERT INTO service_ver (pkey, fkey_svc, maj_ver, min_ver)
-VALUES (3, (SELECT pkey FROM service WHERE svc_id = 'WEBUI'), 1, 0);
-
-INSERT INTO service_ver (pkey, fkey_svc, maj_ver, min_ver)
-VALUES (4, (SELECT pkey FROM service WHERE svc_id = 'SCHED'), 1, 0);
-
+-- 
 -- Bind some services to some versions
+-- 
 
 INSERT INTO product_service (pkey, fkey_prod_ver, fkey_svc_ver)
-VALUES ( 0, 14 /* WEBDEV 2.0 */,  0 /* AUTH 1.0 */);
+VALUES ( 0, 0 /* CPLANE 0.0 */,  0 /* AUTH 1.0 */);
 
 INSERT INTO product_service (pkey, fkey_prod_ver, fkey_svc_ver)
-VALUES ( 1, 14 /* WEBDEV 2.0 */,  3 /* WEBUI 1.0 */);
+VALUES ( 2, 4 /* AION 0.1 */,  0 /* AUTH 1.0 */);
 
 INSERT INTO product_service (pkey, fkey_prod_ver, fkey_svc_ver)
-VALUES ( 2, 8 /* UTUNE 1.2 */,  0 /* AUTH 1.0 */);
+VALUES ( 3, 4 /* AION 0.1 */,  1 /* SHIFTS 0.0 */);
 
 INSERT INTO product_service (pkey, fkey_prod_ver, fkey_svc_ver)
-VALUES ( 3, 8 /* UTUNE 1.2 */,  3 /* WEBUI 1.0 */);
+VALUES ( 4, 0 /* CPLANE 0.0 */,  1 /* CPUI 0.0 */);
 
-INSERT INTO product_service (pkey, fkey_prod_ver, fkey_svc_ver)
-VALUES ( 4, 8 /* UTUNE 1.2 */,  1 /* OSTORE 1.0 */);
 
 
