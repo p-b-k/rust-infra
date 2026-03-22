1abb5ec8d76a2ab65254467596442af632d9d7a3
Paul B. Krause <paul.b.krause@gmail.com>
2026-03-22 13:38:38 -0400

    Startign to put page cache together

--- rust/ui/src/pagecache.rs
+++ rust/ui/src/pagecache.rs
@@ -2,7 +2,7 @@
 // Template Based Cache
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-use crate::rescache::CacheEntry;
+use crate::rescache::{CacheEntry, CacheLogic, CacheState, ResCache};
 
 pub struct Page {
     pub id: String,
@@ -12,3 +12,35 @@ pub struct Page {
 }
 
 pub type PageCacheEntry = CacheEntry<Page>;
+
+pub enum PageField {
+    Title,
+    Icon,
+    Help,
+    Desc,
+}
+
+pub enum Part {
+    Text(String),
+    Field(PageField),
+}
+
+pub struct PageData {
+    pub title: String,
+    pub icon: String,
+    pub help: String,
+    pub desc: String,
+}
+
+pub struct PageCacheState {
+    pub parts: Vec<Part>,
+}
+
+impl CacheState for PageCacheState {
+    fn needs_sync(&self) -> bool;
+    fn sync(&mut self) -> Option<String>;
+}
+
+pub struct PageCacheLogic {}
+
+pub type PageCache = ResCache<PageCacheState, PageData, PageCacheLogic>;
