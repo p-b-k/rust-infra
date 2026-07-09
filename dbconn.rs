279968439af9734ecc3e1ba278f33f947668abb1
Paul B. Krause <paul.b.krause@gmail.com>
2026-07-09 16:17:33 -0400

    Adding dbconnection info to infra

---
+++ dbconn.rs
@@ -0,0 +1,24 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Basic DB Config info
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct DbConfig {
+    pub name: String,
+    pub user: String,
+    pub pass: String,
+    pub host: String,
+    pub port: u32,
+}
+
+impl DbConfig {
+    pub fn to_url(&self) -> String {
+        let name = &self.name;
+        let user = &self.user;
+        let pass = &self.pass;
+        let host = &self.host;
+        let port = self.port;
+
+        format!("mysql://{user}:{pass}@{host}:{port}/{name}")
+    }
+}
